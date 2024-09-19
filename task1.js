46948498deff82783900176467b8382bd33d114b
sdt-IlliaShahoferov <ishahoferov@stratadecision.com>
2024-09-19 21:50:23 +0300

    complete tasks

--- task1.js
+++ task1.js
@@ -5,3 +5,20 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+const ulElement = document.getElementsByClassName('usersList')[0];
+const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+const data = await getData('https://jsonplaceholder.typicode.com/users');
+
+data.forEach(item => {
+  const todoLi = document.createElement("li");
+  todoLi.textContent = item.name;
+
+  ulElement.append(todoLi);
+});
--- task2.js
+++ task2.js
@@ -5,3 +5,26 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.getElementById('getUserButton').addEventListener('click', async () => {
+  const userName = document.getElementById('userNameInput').value;
+  const userCityElement = document.getElementById('userCity');
+
+  userCityElement.textContent = '';
+
+  if (!userName) {
+      alert('Please enter a user name');
+      return;
+  }
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const users = await response.json();
+
+  const user = users.find(user => user.name === userName);
+
+  if (user) {
+      userCityElement.textContent = user.address.city;
+  } else {
+      userCityElement.textContent = 'User not found';
+  }
+});
